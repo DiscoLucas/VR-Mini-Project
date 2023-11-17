efc495607908578b9aa0336875f9101f5f9597b1
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2023-11-17 14:53:33 +0100

    Imported XR simulator and did work on patty script

--- Assets/Undercooked/Scripts/Patty.cs
+++ Assets/Undercooked/Scripts/Patty.cs
@@ -6,21 +6,85 @@ public class Patty : MonoBehaviour
 {
     public GameObject stoveTop;
     public float timeCooked;
+    public float finishTime;
+    public float finishWindow; // the window of time where the patty is considered cooked, before it burns
+    public Color raw;
+    public Color cooked;
+    private new Renderer renderer;
+    public PattyState currentState;
 
-
-    public void OnTriggerEnter(Collider other)
+    // Patty enum states
+    public enum PattyState
     {
-        Debug.Log("triggerd");
+        RawState,
+        CookedState,
+        BurntState
     }
+
+
+    public void OnTriggerStay(Collider other) //when the patty enters the stove
+    {
+        if (other.gameObject == stoveTop) //if the patty is on the stove
+        {
+            timeCooked += Time.deltaTime; //add time to the timer
+            float cookingProgress = Mathf.Clamp01(timeCooked / finishTime); //clamp the time to the finish time
+
+            Color lerpedColor = Color.Lerp(raw, cooked, cookingProgress); //lerp the color of the patty
+            renderer.material.color = lerpedColor;
+
+            if (timeCooked >= finishTime)
+            {
+                if (timeCooked >= finishTime + finishWindow) //if the patty is cooked
+                {
+                    SetState(PattyState.BurntState);
+                }
+                else
+                {
+                    SetState(PattyState.CookedState);
+                }
+            }
+            //gameObject.GetComponent<Renderer>().material.color = Color.Lerp(raw, cooked, finishTime); //change the color of the patty
+
+            /*if (timeCooked >= finishTime)
+            {
+                Debug.Log("Patty is cooked");
+                if (timeCooked >= finishTime + finishWindow) //if the patty is cooked
+                {
+                    Debug.Log("Patty is burnt");
+                    gameObject.GetComponent<Renderer>().material.color = Color.Lerp(cooked, Color.black, 5f); //
+                }
+            }
+            */
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();
+        Color raw = renderer.material.color;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
         
+    }
+    private void SetState(PattyState newState)
+    {
+        currentState = newState;
+
+        switch (currentState)
+        {
+            case PattyState.CookedState:
+                Debug.Log("Patty is cooked");
+                break;
+
+            case PattyState.BurntState:
+                Debug.Log("Patty is burnt");
+                renderer.material.color = Color.Lerp(cooked, Color.black, 5f);
+                break;
+        }
     }
 }
